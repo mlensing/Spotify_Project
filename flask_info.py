b71fb076898b9e39268e50e28d05cfc54bcdd293
mlensing <68037596+mlensing@users.noreply.github.com>
2021-08-16 18:00:04 -0400

    changed name

---
+++ flask_info.py
@@ -0,0 +1,54 @@
+
+
+from flask import Flask
+from flask_cors import CORS
+import pandas as pd
+import json
+
+from recommender import return_song_suggestion
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask('app')
+CORS(app)
+print(__name__)
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to my Spotify Recommender API!<br/>")
+
+@app.route("/api/search/<query>")
+def search_query(query=None):
+
+    print('it is working')
+    
+    try:
+        print('ttest1')
+        results = return_song_suggestion(query)
+        print('test2')
+        print('test3')
+        data = json.dumps(results, ensure_ascii=False, indent=4)
+        print("returning data")
+
+        return (
+            data
+        )
+
+    except Exception as e:
+        print('exception')
+        return (
+            f"{e}"
+    )
+
+if __name__ == '__main__':
+    app.run()
