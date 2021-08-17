3cbbcb2b9b95b167f5c780916658218a39a30029
mlensing <68037596+mlensing@users.noreply.github.com>
2021-08-16 21:10:26 -0400

    Remove dependency on shelly code

--- flask_info.py
+++ flask_info.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 import pandas as pd
 import json
 
-from recommender import return_song_suggestion
+# from recommender import return_song_suggestion
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -15,7 +15,7 @@ pd.set_option('display.max_colwidth', None)
 #################################################
 # Flask Setup
 #################################################
-app = Flask('app')
+app = Flask(__name__)
 CORS(app)
 print(__name__)
 #################################################
@@ -34,9 +34,10 @@ def search_query(query=None):
     
     try:
         print('ttest1')
-        results = return_song_suggestion(query)
+        # results = return_song_suggestion(query)
         print('test2')
         print('test3')
+        results = {"song": "My song"}
         data = json.dumps(results, ensure_ascii=False, indent=4)
         print("returning data")
 
