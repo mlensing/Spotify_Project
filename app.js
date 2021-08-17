d429a136680e5a44da2cc1889988f9663d1985ca
mlensing <68037596+mlensing@users.noreply.github.com>
2021-08-17 16:04:22 -0400

    adding javascript

---
+++ app.js
@@ -0,0 +1,29 @@
+function handleClickSearch() {
+
+    const query = d3.select('#search_query').property("value");
+
+    if (query) {
+        console.log(query);
+        document.getElementById("search_query").placeholder=query;
+
+        var url = "https://spotify-heroku-api.herokuapp.com/api/search/"; 
+        var updated_url = url + query;
+
+        fetch(updated_url)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+
+                songData = data;
+                buildTable(songData);
+                var num_query_results = songData.length;
+
+                document.getElementById("search_num").innerHTML = "Your new song recommendation is " + songData;
+          })
+
+          .catch(function (err) {
+            console.log(err);
+          });
+    }
+}
