5b5b5c725adc4e310e474111d787d7e8d2fe23cc
mlensing <68037596+mlensing@users.noreply.github.com>
2021-08-17 22:20:13 -0400

    no if else

--- app.js
+++ app.js
@@ -18,17 +18,14 @@ function handleClickSearch() {
                 songData = data;
                 console.log(songData)
          
-                if (songData==undefined){
-                  document.getElementById("search_num").innerHTML = "Your song is invalid. Please try again.";
-                }
-                else{
+                
                   document.getElementById("search_num").innerHTML = "Your new song recommendation is " + songData['Song'] + " by " + songData['Artist'] + "!";
-                }
 
                 
           })
 
           .catch(function (err) {
+            document.getElementById("search_num").innerHTML = "Your song is invalid. Please try again.";
             console.log(err);
           });
     }
